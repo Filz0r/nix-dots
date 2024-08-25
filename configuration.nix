ce37d6f1a7d538024766e6ebdf23087ccbdd7666
filipe <filipefig26@gmail.com>
2024-08-25 21:56:55 +0100

    fixing zsh pt9

--- configuration.nix
+++ configuration.nix
@@ -110,6 +110,9 @@ services.smartd = {
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # Enable zsh
+  programs.zsh.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.filipe = {
     isNormalUser = true;
