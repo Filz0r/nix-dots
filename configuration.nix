4a0152c61c76b56d1616d4fa6cfac7d26cff31e2
filipe <filipefig26@gmail.com>
2024-09-01 12:25:47 +0100

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -142,15 +142,6 @@ services.smartd = {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-#  environment.systemPackages = with pkgs; [
-#   spotify
-#   slack
-#   discord
-#   jellyfin-media-player
-#   stremio
-#  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
